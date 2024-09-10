117e89a20de0f8741d25628bda8e5d50f2cd834d
unknown <vardhan70139@gnail.com>
2024-09-10 19:30:28 +0530

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
+    target: 'esnext'
+  }
 });
 
 
